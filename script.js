6d068328346b350a09858b4a415108b53c276844
joaoxmarcello <104221541+joaoxmarcello@users.noreply.github.com>
2023-09-30 16:05:23 -0300

    faq js add

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
+const activeClass = 'ativo' ;
+
+function initTabNav() {
 const tabAnimais = document.querySelectorAll('.js-tabAnimais li');
 const tabContent = document.querySelectorAll('#js-tabContent section');
-tabContent[0].classList.add('ativo');
+tabContent[0].classList.add(activeClass);
 
 if(tabAnimais.length && tabContent.length){
   function activeTab(index) {
     tabContent.forEach((section) => {
-      section.classList.remove('ativo');
+      section.classList.remove(activeClass);
     });
-    tabContent[index].classList.add('ativo');
+    tabContent[index].classList.add(activeClass);
   }
   tabAnimais.forEach((item, index) => {
     item.addEventListener('click',() => {
       activeTab(index);
     })
   });
+}}
+initTabNav();
+
+function initFaqAccordion(){
+const accordionList = document.querySelectorAll('.js-faq dt');
+
+if(accordionList.length){
+accordionList[0].classList.add(activeClass);
+accordionList[0].nextElementSibling.classList.add(activeClass);
+
+function activeAccordion () {
+ this.classList.toggle(activeClass)
+ this.nextElementSibling.classList.toggle(activeClass);
 }
+
+accordionList.forEach((item) => {
+  item.addEventListener("click", activeAccordion);
+})
+}}
+initFaqAccordion();
